8f825e809b56320a0b0040254f1781ef113a8d49
vV <hugoruellet@gmail.com>
2020-09-14 16:50:34 +0200

    Integrating every part

--- main.py
+++ main.py
@@ -14,34 +14,43 @@ from tensorflow.keras.models import load_model
 from SudokuDLX.SudokuDLX import SudokuDLX
 from Utils.display import Display
 from Utils.extractor import FeatureExtractor
+from Utils.tracker import Tracker
+from AR.KPExtractor import KPExtractor
+
 
 
 if __name__ == "__main__":
-  #Const
-  SSIM_TRESHOLD = .6
   R_H,R_W = 288,288
   CELL_SIZE = 28
+
+  W,H = 640,480
+  #FOCAL
+  F = 800
+  #INTRINSIC CAMERA MATRIX
+  K = np.array([[F,0,W//2],[0,F,H//2],[0,0,1]])
+
+  fn_pattern = "./pattern2.jpg"
 
   # Load Classification model
   # model_path = "./Model/Output/optimized.h5"
   model_path = "./Model/Output/tmp/cp-38-0.0509.h5"
   sudoku_model = load_model(model_path)
 
-  # Load Sudoku pattern to make sure it's a sudoku we've detected
-  pat = cv.cvtColor(cv.imread("./pattern2.jpg"),cv.COLOR_BGR2GRAY)
-  h,w = pat.shape
 
-  #Instanciate Displayer and Extractor
+  #Instanciate Classes
   di = Display()
   ex = FeatureExtractor()
+  tr = Tracker(fn_pattern)
+  kp = KPExtractor(K)
 
-  FOUND = False
+  ui = None
+
+  SOLVED = False
 
   cap = cv.VideoCapture(0)
   while True:
     s_loop = time()
     ret,orig = cap.read()
-
     if not ret:
       break
 
@@ -56,80 +65,83 @@ if __name__ == "__main__":
     if puzzle is not None:
 
       #Unwrap it
+      warp_orig = ex.unwrap(orig,puzzle)
       warp_thresh = ex.unwrap(thresh,puzzle)
       warp = ex.unwrap(gray,puzzle)
 
-      #Prepare for SSIM comparison
-      warp_cmp = cv.bitwise_not(warp_thresh)
-      warp_cmp = cv.resize(warp_cmp,(w,h),interpolation=cv.INTER_AREA)
-
-      #Compare and rescale score to 0..1
-      score_ssim = ssim(warp_cmp,pat, data_range=pat.max() - pat.min())
-      score_ssim = score_ssim*.5 +.5
-
-
       #If it looks like a sudoku
-      if score_ssim > SSIM_TRESHOLD:
-        cv.drawContours(orig,[puzzle],0,(255,0,0),3)
+      if tr.compare(warp_thresh):
+        tr.puzzle = puzzle
         #If we alread found and decoded it skip
-        if FOUND: continue
+        if SOLVED:
+          if tr.last_puzzle is not None:
+            #Grab Tracking data
+            src_pts = tr.get_cnts_points(True)
+            dst_pts = tr.get_cnts_points(False)
 
-        #Resize to Classifier input size
-        warp = cv.resize(warp,(R_W,R_H))
+            #Build homography and reprojection matrix
+            H,mask = cv.findHomography(src_pts,dst_pts,cv.RANSAC,5.0)
+            matrix = kp.project_matrix(H)
 
-        #Retrieve Sudoku cells
-        cells = ex.extract_cells(warp)
-        cells = ex.threshold_cells(cells)
-        cells,digits = ex.clear_cells(cells)
-
-        #Ui for debugging purpose
-        ui = ex.ui(cells)
-
-        #TODO: Look under
-        preds = {}
-
-        #Initiate Sudoku Empty board
-        board = [[0 for _ in range(9)] for _ in range(9)]
-        for i,(c,d) in enumerate(zip(cells,digits)):
-          #If digit in cell
-          if d:
-            roi = cv.resize(c,(CELL_SIZE,CELL_SIZE))
-            roi = roi.astype("float") / 255.0
-            roi = img_to_array(roi)
-            roi = np.expand_dims(roi,axis=0)
-
-            #Predict Digit
-            #If even with a good model we didn't manage to predict correctly,
-            #Run X preds and select best for each to smooth potential errors (6/8/9),(1/7)...
-            prediction = sudoku_model.predict(roi).argmax(axis=1)[0]
-            board[i//9][i%9] = prediction
+            #Render
+            orig = di.render_warp(orig,ui,matrix,ui_pts,src_pts,dst_pts)
         
-        #Run SudokuDLX with the predicted board
-        sudoku = SudokuDLX()
-        SudokuDLX.print_solution(board)
-        solved = sudoku.solve(board,True)
-        if solved[0]:
-          orig_warp = ex.unwrap(orig,puzzle)
-          orig_warp = cv.resize(orig_warp,(R_W,R_H))
-          drawed_warp = di.draw(orig_warp,ex.extract_cells(orig_warp),solved[1])
-          FOUND = True
-          cv.imshow("draw_warp",drawed_warp)
+        else:
+          #Resize to Classifier input size
+          warp = cv.resize(warp,(R_W,R_H))
+
+          #Retrieve Sudoku cells
+          cells = ex.extract_cells(warp)
+          cells = ex.threshold_cells(cells)
+          cells,digits = ex.clear_cells(cells)
+
+          #Ui for debugging purpose
+          # ui = ex.ui(cells)
+
+          #TODO: Look under
+          preds = {}
+
+          #Initiate Sudoku Empty board
+          board = [[0 for _ in range(9)] for _ in range(9)]
+          for i,(c,d) in enumerate(zip(cells,digits)):
+            #If digit in cell
+            if d:
+              roi = cv.resize(c,(CELL_SIZE,CELL_SIZE))
+              roi = roi.astype("float") / 255.0
+              roi = img_to_array(roi)
+              roi = np.expand_dims(roi,axis=0)
+
+              #Predict Digit
+              #If even with a good model we didn't manage to predict correctly,
+              #Run X preds and select best for each to smooth potential errors (6/8/9),(1/7)...
+              prediction = sudoku_model.predict(roi).argmax(axis=1)[0]
+              board[i//9][i%9] = prediction
 
 
-
-          #TODO: AR Part here
           
-          cv.waitKey()
+          #Run SudokuDLX with the predicted board
+          sudoku = SudokuDLX()
+          SudokuDLX.print_solution(board)
+          solved = sudoku.solve(board,True)
+          if solved[0]:
 
-        # Debug
-        # cv.imshow("warp",np.hstack([warp,ui]))
+            ui = di.build_sudoku_ui(tr.pattern,500,500,solved[1])
+            ui = cv.rotate(ui,cv.ROTATE_90_COUNTERCLOCKWISE)
+            ui_pts = np.float32([[0, 0], [0, ui.shape[0] - 1], [ui.shape[1] - 1, ui.shape[0] - 1], [ui.shape[1] - 1, 0]]).reshape(-1, 1, 2)
+
+            SOLVED = True
+
+
+        tr.last_puzzle = tr.puzzle
+          
 
 
 
-    if di.show([orig]):
+
+    loop_time = time()-s_loop
+    if di.show(a=[orig]):
       break
     
-    loop_time = time()-s_loop
     # print(f"Looped in {loop_time:.3f} secs")
 
 
